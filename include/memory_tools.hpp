dfff3774239722b2197a5a48c9874e6b27dbc8cd
DymOK <dym93ok@yandex.ru>
2020-11-12 00:19:10 +0300

    Updated memory tools

--- include/memory_tools.hpp
+++ include/memory_tools.hpp
@@ -1,18 +1,17 @@
 #pragma once
 #include <heap.h>
-#include <memory>
+#include <utility.hpp>
+#include <unique_ptr.hpp>
 
 namespace winapi::kernel::mm {
-	
 template <class Ty, class... Types>
 constexpr Ty* construct_at(Ty* place, Types&&... args) {
-  return ::new (const_cast<void*>(static_cast<const volatile void*>(place)))
-      Ty(std::forward<Types>(args)...);
+  return new (const_cast<void*>(static_cast<const volatile void*>(place)))
+      Ty(forward<Types>(args)...);
 }
 
 template <class Ty>
 constexpr void destroy_at(Ty* object_ptr) {
   std::destroy_at(object_ptr);
 }
-
 }
--- include/raii.hpp
+++ include/raii.hpp
@@ -2,12 +2,12 @@
 #include <memory_tools.hpp>
 
 #include <ntddk.h>
-#include <type_traits>
+
 
 namespace winapi::kernel::raii {
 auto MakeHandleGuard(HANDLE handle) {
   auto guard{[](HANDLE target) { ObDereferenceObject(target); }};
-  return std::unique_ptr<std::remove_pointer_t<HANDLE>, decltype(guard)>(handle,
+  return winapi::kernel::unique_ptr<std::remove_pointer_t<HANDLE>, decltype(guard)>(handle,
                                                                          guard);
 }
 using handle_guard = std::invoke_result_t<decltype(MakeHandleGuard), HANDLE>;
