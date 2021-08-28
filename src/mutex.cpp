37569e4424c1a186fa571c08dd9e0b9d1aa7f5d8
DymOK93 <dym93ok@yandex.ru>
2021-08-29 00:20:50 +0300

    Implemented spin_lock::try_lock()

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -3,66 +3,51 @@
 
 #include <ntddk.h>
 
-namespace ktl {
-namespace th::details {
-void spin_lock_policy<SpinlockType::Apc>::lock(
-    KSPIN_LOCK& target) const noexcept {
-  KeAcquireSpinLock(addressof(target), addressof(m_prev_irql));
-}
-
-void spin_lock_policy<SpinlockType::Apc>::unlock(
-    KSPIN_LOCK& target) const noexcept {
-  KeReleaseSpinLock(addressof(target), m_prev_irql);
-}
-
-void spin_lock_policy<SpinlockType::Dpc>::lock(
+namespace ktl::th::details {
+void spin_lock_policy<SpinlockType::DpcOnly>::lock(
     KSPIN_LOCK& target) const noexcept {
   KeAcquireSpinLockAtDpcLevel(addressof(target));
 }
 
-void spin_lock_policy<SpinlockType::Dpc>::unlock(
+bool spin_lock_policy<SpinlockType::DpcOnly>::try_lock(
+    KSPIN_LOCK& target) const noexcept {
+  return KeTryToAcquireSpinLockAtDpcLevel(addressof(target));
+}
+
+void spin_lock_policy<SpinlockType::DpcOnly>::unlock(
     KSPIN_LOCK& target) const noexcept {
   KeReleaseSpinLockFromDpcLevel(addressof(target));
 }
 
 void spin_lock_policy<SpinlockType::Mixed>::lock(
     KSPIN_LOCK& target) const noexcept {
-  if (const irql_t irql = get_current_irql(); irql < DISPATCH_LEVEL) {
-    KeAcquireSpinLock(addressof(target), addressof(m_prev_irql));
-  } else {
-    KeAcquireSpinLockAtDpcLevel(addressof(target));
-    m_prev_irql = irql;
+  m_prev_irql = KeAcquireSpinLockRaiseToDpc(addressof(target));
+}
+
+bool spin_lock_policy<SpinlockType::Mixed>::try_lock(
+    KSPIN_LOCK& target) const noexcept {
+  const irql_t prev_irql{raise_irql(DISPATCH_LEVEL)};
+  if (!KeTryToAcquireSpinLockAtDpcLevel(addressof(target))) {
+    lower_irql(prev_irql);
+    return false;
   }
+  m_prev_irql = prev_irql;
+  return true;
 }
 
 void spin_lock_policy<SpinlockType::Mixed>::unlock(
     KSPIN_LOCK& target) const noexcept {
-  if (m_prev_irql < DISPATCH_LEVEL) {
-    KeReleaseSpinLock(addressof(target), m_prev_irql);
-  } else {
-    KeReleaseSpinLockFromDpcLevel(addressof(target));
-  }
+  KeReleaseSpinLock(addressof(target), m_prev_irql);
 }
 
-void queued_spin_lock_policy<SpinlockType::Apc>::lock(
-    KSPIN_LOCK& target,
-    KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
-  KeAcquireInStackQueuedSpinLock(addressof(target), addressof(queue_handle));
-}
-
-void queued_spin_lock_policy<SpinlockType::Apc>::unlock(
-    KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
-  KeReleaseInStackQueuedSpinLock(addressof(queue_handle));
-}
-
-void queued_spin_lock_policy<SpinlockType::Dpc>::lock(
+void queued_spin_lock_policy<SpinlockType::DpcOnly>::lock(
     KSPIN_LOCK& target,
     KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
   KeAcquireInStackQueuedSpinLockAtDpcLevel(addressof(target),
                                            addressof(queue_handle));
 }
 
-void queued_spin_lock_policy<SpinlockType::Dpc>::unlock(
+void queued_spin_lock_policy<SpinlockType::DpcOnly>::unlock(
     KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
   KeReleaseInStackQueuedSpinLockFromDpcLevel(addressof(queue_handle));
 }
@@ -70,21 +55,11 @@ void queued_spin_lock_policy<SpinlockType::Dpc>::unlock(
 void queued_spin_lock_policy<SpinlockType::Mixed>::lock(
     KSPIN_LOCK& target,
     KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
-  if (get_current_irql() < DISPATCH_LEVEL) {
-    KeAcquireInStackQueuedSpinLock(addressof(target), addressof(queue_handle));
-  } else {
-    KeAcquireInStackQueuedSpinLockAtDpcLevel(addressof(target),
-                                             addressof(queue_handle));
-  }
+  KeAcquireInStackQueuedSpinLock(addressof(target), addressof(queue_handle));
 }
 
 void queued_spin_lock_policy<SpinlockType::Mixed>::unlock(
     KLOCK_QUEUE_HANDLE& queue_handle) const noexcept {
-  if (queue_handle.OldIrql < DISPATCH_LEVEL) {
-    KeReleaseInStackQueuedSpinLock(addressof(queue_handle));
-  } else {
-    KeReleaseInStackQueuedSpinLockFromDpcLevel(addressof(queue_handle));
-  }
+  KeReleaseInStackQueuedSpinLock(addressof(queue_handle));
 }
-}  // namespace th::details
-}  // namespace ktl
+}  // namespace ktl::th::details
